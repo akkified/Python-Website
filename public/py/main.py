236a12e79ef087f364cbe63b2955783d04390760
akkified <115843366+akkified@users.noreply.github.com>
2024-01-26 12:07:42 -0500

    calc

--- public/py/main.py
+++ public/py/main.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+from pyweb import pydom
+
+def calc(num1,num2):
+    operation = input("What operation do want to do? ")
+    if operation == "Add":
+        return(num1 + num2)
+    elif operation == "Subtract":
+        return(num1 - num2)
+    elif operation == "Multiply":
+        return(num1 * num2)
+    elif operation == "Divide":
+        return(num1/num2)
+    else:
+        print("You entered the wrong operation!")
+
+print(calc(float(input("What is your first number? ")),float(input("What is your second number? "))))
